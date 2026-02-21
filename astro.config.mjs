1e64ae10acf73121a8da98ba26a14f65aeb3315c
Axel08-IHQ <axel.16hueritaz@gmail.com>
2026-02-21 18:04:13 -0500

    Nuevas Actualizacion para Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://Axel08-IHQ.github.io",
+    site: "https://axel08-ihq.github.io",
     base: "/PortafolioP/"
 });
